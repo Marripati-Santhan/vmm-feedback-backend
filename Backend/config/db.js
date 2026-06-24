4cf16715f22107cb409fd4758f9410d9b66849f3
Santhan Marripati <marripatisanthan@gmail.com>
2026-06-24 14:42:44 +0530

    Connect backend to Aiven database

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
 const connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "j0hnwick123",
-  database: "vmm_feedback_system",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
