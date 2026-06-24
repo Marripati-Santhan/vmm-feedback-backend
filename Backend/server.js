c3cc7e22ad23458e8e82d3e1b9030dbe1971c05f
Santhan Marripati <marripatisanthan@gmail.com>
2026-06-24 14:22:21 +0530

    Prepare backend for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const storeFeedbackRoutes = require("./routes/storeFeedbackRoutes");
 const employeeRoutes = require("./routes/employeeRoutes");
+
 const staffReviewRoutes =
   require("./routes/staffReviewRoutes");
 
