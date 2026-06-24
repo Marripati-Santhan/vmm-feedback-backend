9bbc20760a8cc2c70406cdb8d1cd9f7d295ba4f7
Santhan Marripati <marripatisanthan@gmail.com>
2026-06-24 14:47:29 +0530

    Add Render port support

--- Backend/server.js
+++ Backend/server.js
@@ -26,7 +26,7 @@ app.get("/", (req, res) => {
   res.send("VMM Feedback Backend Running...");
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
